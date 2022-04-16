7d24d3debdfd642a3dade1596b59d1a917b28e90
abdealijaroli <abdealiburu03@gmail.com>
2022-04-17 01:25:15 +0530

    worked on loader comp

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Loader from '../components/Loader'
 
 export default function Home() {
    return (
       <div>
+         <Loader show />
          Home Page
       </div>
    )
