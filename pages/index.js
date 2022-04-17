4de28ad408d6af8117d75b30a8e3ceeaf13a53fe
abdealijaroli <abdealiburu03@gmail.com>
2022-04-17 17:29:12 +0530

    implemented toaster and fixed navbar

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Loader from '../components/Loader'
+import toast from 'react-hot-toast'
 
 export default function Home() {
    return (
-      <div>
-         <Loader show />
+      <div className={styles.container}>
+         <Loader show={false} />
          Home Page
+         <button onClick={() => toast.success("hacked the world!")}>click me!</button>
       </div>
    )
 }
