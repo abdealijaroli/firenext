91718f62ac812a9f0069d4a589f15de5367d1c9e
abdealijaroli <abdealiburu03@gmail.com>
2022-05-05 15:46:41 +0530

    auth implemented

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,15 @@ import '../styles/globals.css'
 import Navbar from '../components/Navbar'
 import { Toaster } from 'react-hot-toast'
 import { UserContext } from '../lib/context'
+import { useUserData } from '../lib/hooks'
 
 function MyApp({ Component, pageProps }) {
+
+   const userData = useUserData(); 
+   
    return (
       <>
-         <UserContext.Provider value={{user: {}, username: "abde"}}>
+         <UserContext.Provider value={userData}>
             <Navbar />
             <Component {...pageProps} />
             <Toaster />
